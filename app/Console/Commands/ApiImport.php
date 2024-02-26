791f2d4e910dcb00ef37222a17d4c1f8d72560f9
Connah Fearnley <connah@cmcrm.co.uk>
2024-02-26 00:20:58 +0000

    BF-4 #comment Add Commands for Imports from API and CSV #time 4h

--- app/Console/Commands/ApiImport.php
+++ app/Console/Commands/ApiImport.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Category;
+use App\Repositories\CategoryRepository;
+use App\Repositories\ProductRepository;
 use Illuminate\Console\Command;
 
 class ApiImport extends Command
@@ -11,20 +14,67 @@ class ApiImport extends Command
      *
      * @var string
      */
-    protected $signature = 'import:csv';
+    protected $signature = 'import:api {url?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import data from a CSV file (usually called orders.csv)';
+    protected $description = 'Import data from an API URL';
+
+    /**
+     * The default URL we will pull the API import from
+     * This hardcoded but overridable value allows for easier API swapout without codebase updates
+     * 
+     * @var string
+     */
+    protected $url = 'https://nt5gkznl19.execute-api.eu-west-1.amazonaws.com/prod/products';
+
+    protected $categoryRepository;
+    protected $productRepository;
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $this->categoryRepository = new CategoryRepository();
+        $this->productRepository = new ProductRepository();
+
+        if ($this->argument('url')) {
+            $this->url = $this->argument('url');
+        }
+
+        $this->line("Pulling API data from: ".$this->url);
+
+        while (!is_null($this->url)) {
+            $data = json_decode(file_get_contents($this->url), true);
+            $this->line('A page of data has been obtained. Products found.');
+
+            foreach($data["value"] as $product) {
+                $category = $this->categoryRepository->findOrCreate($product["category"]);
+                $this->line('Found or Created Category: '.$category->name);
+
+                $saved = $this->productRepository->findOrCreate(
+                    $this->formatProduct($product, $category)
+                );
+                $this->line('Found or Created Product: '.$saved->sku);
+            }
+
+            $this->url = $data["@odata.nextLink"];
+        }
+
+        $this->info('Completed');
+        return 0; // Zero errors
+    }
+
+    private function formatProduct(array $product, Category $category)
+    {
+        // Attach Category ID
+        $product["category_id"] = $category->id;
+        unset($product["category"]);
+
+        return $product;
     }
 }
--- app/Console/Commands/CsvImport.php
+++ app/Console/Commands/CsvImport.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Customer;
+use App\Repositories\CustomerRepository;
+use App\Repositories\OrderRepository;
 use Illuminate\Console\Command;
 
 class CsvImport extends Command
@@ -11,20 +14,69 @@ class CsvImport extends Command
      *
      * @var string
      */
-    protected $signature = 'import:csv';
+    protected $signature = 'import:csv {file?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import data from an API endpoint';
+    protected $description = 'Import data from a CSV file (default: orders.csv)';
+
+    protected $file = 'orders.csv';
+
+    protected $customerRepository;
+    protected $orderRepository;
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $this->customerRepository = new CustomerRepository();
+        $this->orderRepository = new OrderRepository();
+
+        // Use file argument if we have one otherwise use default filename
+        $this->file = "./storage/".($this->argument('file') ?: $this->file);
+        $this->line("Pulling order data from: ".$this->file);
+
+        $row = 0; // Header, 1 and above is for data
+        if (($handle = fopen($this->file, "r")) !== false) {
+            while (($data = fgetcsv($handle)) !== false) {
+                if ($row > 0) { // If this is not the header row
+                    $saved = $this->orderRepository->addOrderItem(
+                        $this->mapData($data)
+                    );
+
+                    $this->line("Added Order Item: ".$data[0]."-".$data[2]);
+                }
+
+                $row++;
+            }
+
+            fclose($handle);
+        }
+
+        $this->info('Complete');
+        return 0;
+    }
+
+    /**
+     * Add / find the customer and
+     * Map the data array to store the order record
+     */
+    private function mapData($data)
+    {
+        $customer = $this->customerRepository->findOrCreate($data[1]);
+        $this->line("Customer: ".$customer->id." - ".$customer->name);
+
+        $order = $this->orderRepository->findOrCreate($data[0], $customer->id);
+        $this->line("Order: ".$order->id);
+
+        return [
+            "order_id" => $order->id,
+            "sku" => $data[2],
+            "quantity" => $data[3],
+        ];
     }
 }
